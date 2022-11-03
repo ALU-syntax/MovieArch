1ef9aa01f65b089cc60315017ae7fb97b32474ff
ALU-syntax <ardianiqbal40@gmail.com>
2022-11-04 02:15:18 +0700

    add image view on profile activity

--- app/src/main/java/com/ardian/moviearch/ProfileActivity.kt
+++ app/src/main/java/com/ardian/moviearch/ProfileActivity.kt
@@ -1,6 +1,8 @@
 package com.ardian.moviearch
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ardian.moviearch.databinding.ActivityProfileBinding
@@ -14,6 +16,64 @@ class ProfileActivity : AppCompatActivity() {
         binding = ActivityProfileBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.ivGithub.setOnClickListener{
+            val uri = Uri.parse("https://github.com/ALU-syntax")
+            try {
+                Intent(Intent.ACTION_VIEW, uri).apply {
+                    setPackage("com.github.android")
+                    startActivity(this)
+                }
+            } catch (e : ActivityNotFoundException){
+                Intent(Intent.ACTION_VIEW, uri).apply {
+                    startActivity(this)
+                }
+            }
+        }
+
+        binding.ivTwitter.setOnClickListener {
+            val uri = Uri.parse("https://twitter.com/mengkerebe")
+            try {
+                Intent(Intent.ACTION_VIEW, uri).apply {
+                    setPackage("com.twitter.android")
+                    startActivity(this)
+                }
+            } catch (e : ActivityNotFoundException){
+                Intent(Intent.ACTION_VIEW, uri).apply {
+                    startActivity(this)
+                }
+            }
+        }
+
+        binding.ivInstagram.setOnClickListener {
+            val uri = Uri.parse("https://instagram.com/ardian_iqbal_?igshid=YmMyMTA2M2Y=")
+            try {
+                Intent(Intent.ACTION_VIEW, uri).apply {
+                    setPackage("com.instagram.android")
+                    startActivity(this)
+                }
+            } catch (e : ActivityNotFoundException){
+                Intent(Intent.ACTION_VIEW, uri).apply {
+                    startActivity(this)
+                }
+            }
+        }
+
+        binding.ivLinkedin.setOnClickListener {
+            val uri = Uri.parse("https://www.linkedin.com/in/ardianiqbal/")
+            try {
+                Intent(Intent.ACTION_VIEW, uri).apply {
+                    setPackage("com.linkedin.android")
+                    startActivity(this)
+                }
+            } catch (e : ActivityNotFoundException){
+                Intent(Intent.ACTION_VIEW, uri).apply {
+                    startActivity(this)
+                }
+            }
+
+        }
+
 
         binding.btnBack.setOnClickListener{
             Intent(this, MainActivity::class.java).apply {
